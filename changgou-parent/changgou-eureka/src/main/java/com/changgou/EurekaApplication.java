0a6737a38563de253684b909e13a33bbeaea22a4
倚栏听风 <1845706088@qq.com>
2021-02-01 21:04:59 +0800

    day 01

---
+++ changgou-parent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 今何许
+ * @date 2021-02-01 15:14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/GoodsApplication.java
@@ -0,0 +1,19 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author 今何许
+ * @date 2021-02-01 17:04
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan(basePackages = {"com.changgou.goods.dao"})
+public class GoodsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsApplication.class);
+    }
+}
